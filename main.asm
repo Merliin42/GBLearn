608ef57acdd35060c682f116885c809a7b3dc1f9
Lucas Dupuis <lucas.dupuis@ffgolf.org>
2019-07-20 12:41:37 +0200

    First commit

---
+++ main.asm
@@ -0,0 +1,79 @@
+INCLUDE "hardware.inc" ; Include hardware specs
+
+SECTION "Header", ROM0[$100] ; create a new section of code, for storage and define used bank as RMS0
+
+EntryPoint:
+	di ; Disable interrupts
+	jp Start ; tells to the processor to jump to start
+
+REPT $150 - $104 ; Repeat an operation $150 - $104 times
+	db 0 ; Writes 0 in the ROM
+ENDR ; End of the REPT loop
+
+SECTION "Game code", ROM0 ; Create a new section of code, who is store whenever the assembler wants
+
+Start:
+	call lcdOff
+
+	ld hl, $9000 ; Set hl to $9000, witch is the pointer to the first VRAM adress
+	ld de, FontTiles ; Set de to the pointer to FontTiles label
+	ld bc, FontTilesEnd - FontTiles ; Set bc to the difference between the start and the end of FontTiles, for making a counter
+
+; Next loop is for copying font to VRAM
+.copyFont ; Label
+	ld a, [de] ; Load in a a byte of FontTiles
+	ld [hli], a ; Load in VRAM ($9000) a byte of FontTiles and increment hl
+	inc de ; Increment the pointer to FontTiles for check the next byte
+	dec bc ; Decrement the counter
+	ld a, b ; load the higher bits of bc to a
+	or c ; set the Z flag if and only if bc == 0
+	jr nz, .copyFont ; if the counter is not 0, loop to copyFont label
+
+	ld hl, $9800 ; Set hl to $9800 witch is the pointer to the first background map
+	ld de, HelloWorldStr ; Set de at a pointer to HelloWorldStr label
+.copyString
+	ld a, [de] ; Load a byte from Hello World string
+	ld [hli], a ; Load the byte from Hello World in a to the first background map
+	inc de ; Increment the pointer to Hello World string
+	and a ; check if the value of a isn't 0 and set the Z flag if it is
+	jr nz, .copyString ; if the string isn't finished, loop on copyString label
+
+	ld a, %11100100 ; Load the color palette in a
+	ld [rBGP], a ; Store it in the BD palette data
+
+	xor a ; ld a, 0
+	ld [rSCY], a ; Reset the position of the screen to Y = 0
+	ld [rSCX], a ; Reset the position of the screen to X = 0
+
+	ld [rNR52], a ; Turn off the volume
+
+	ld a, %10000001 ; Turn on the screen and activate the BG display
+	ld [rLCDC], a ; Load the value in a into LCD Controller
+
+.lockup ; Loop for lockup the processor
+	jr .lockup ; Jump to lockup label
+
+SECTION "Font", ROM0 ; Create a section where we store code
+
+FontTiles:
+INCBIN "font.chr" ; Include binary file
+FontTilesEnd:
+
+SECTION "Hello World string", ROM0 ; Create another section
+
+HelloWorldStr:
+	db "Hello World!", 0 ; Writes in the ROM the phrase in ASCII
+
+SECTION "Functions", ROM0
+
+; Turn off the LCD Screen
+lcdOff:
+; Next loop is for waiting VBlank for turning LCD off
+.waitVBlank ; Label
+	ld a, [rLY] ; Load LCD Controler Y-Coordinate
+	cp 144 ; Compare to 144 for knowing if it is in VBlank period
+	jr c, .waitVBlank ; If it isn't, carry flag is set, so we have to do another loop
+
+	xor a ; ld a, 0
+	ld [rLCDC], a ; Set the LCDC bytes to 0 for turn off the LCD screen
+	ret ; End of the function
---
+++ main2.asm
@@ -0,0 +1,167 @@
+INCLUDE "hardware.inc"
+
+; Defining constants to work with sprite
+_SPR0Y EQU _OAMRAM
+_SPR0X EQU _OAMRAM+1
+_SPR0_NUM EQU _OAMRAM+2
+_SPR0_ATT EQU _OAMRAM+3
+
+; Defining variables to see where we need to move the sprite
+_MOVX EQU _RAM
+_MOVY EQU _RAM+1
+
+SECTION "Header", ROM0[$100]
+
+EntryPoint:
+	di
+	jp Start
+
+REPT $150 - $104
+	db 0
+ENDR
+
+SECTION "Game code", ROM0
+
+Start:
+
+; Turn off LCD screen
+	call lcdOff
+
+; Clean VRAM
+	ld hl, $8000
+	ld bc, $9FFF - $8000
+.cleanVRAM
+	xor a ; ld a, 0
+	ld [hli], a
+	dec bc
+	ld a, b
+	or c
+	jr nz, .cleanVRAM
+
+; Copy tiles to VRAM
+	ld hl, $8000 ; VRAM
+	ld de, tiles
+	ld bc, tilesEnd - tiles
+.copyTiles
+	ld a, [de]
+	ld [hli], a
+	inc de
+	dec bc
+	ld a, b
+	or c
+	jr nz, .copyTiles
+
+	; Create the sprite
+	ld a, 30
+	ld [_SPR0Y], a ; Y pos of the sprite
+	ld [_SPR0X], a ; X pos of the sprite
+	ld a, 1
+	ld [_SPR0_NUM], a ; number of tiles of the table that we will use
+	xor a ; ld a, 0
+	ld [_SPR0_ATT], a ; special attributes, so far nothing
+
+	ld a, %11100100
+	ld [rOBP0], a
+
+	xor a ; ld a, 0
+	ld [rSCY], a
+	ld [rSCX], a
+
+	ld [rNR52], a
+
+	ld a, %10010011
+	ld [rLCDC], a
+
+	; Preparing animation variables
+	ld a, 1
+	ld [_MOVY], a
+	ld [_MOVX], a
+
+animation:
+.wait
+	ld a, [rLY]
+	cp 144
+	jr nz, .wait
+
+	ld a, [_SPR0Y]
+	ld hl, _MOVY
+	add a, [hl]
+	ld hl, _SPR0Y
+	ld [hl], a
+	; compared to see if they change the direction
+	cp 152
+	jr z, .decY
+	cp 16
+	jr z, .incY
+	;do not change
+	jr .endY
+.decY
+	ld a, -1
+	ld [_MOVY], a
+	jr .endY
+.incY
+	ld a, 1
+	ld [_MOVY], a
+.endY
+	; We go with the X, the same but changing the margin
+
+	ld a, [_SPR0X]
+	ld hl, _MOVX
+	add a, [hl]
+	ld hl, _SPR0X
+	ld [hl], a
+	; compared to see if they change the direction
+	cp 160
+	jr z, .decX
+	cp 8
+	jr z, .incX
+	;do not change
+	jr .endX
+.decX
+	ld a, -1
+	ld [_MOVX], a
+	jr .endX
+.incX
+	ld a, 1
+	ld [_MOVX], a
+.endX
+
+	call delay
+	jr animation
+
+
+.lockup
+	jr .lockup
+
+SECTION "Font", ROM0
+
+tiles:
+	db $00, $00, $00, $00, $00, $00, $00, $00
+	db $00, $00, $00, $00, $00, $00, $00, $00
+	db $00, $00, $E0, $EC, $40, $48, $00, $0C
+	db $C0, $00, $AC, $0C, $CC, $0C, $08, $08
+tilesEnd:
+
+SECTION "Functions", ROM0
+
+; Turn off the screen
+lcdOff:
+.waitVBlank
+	ld a, [rLY]
+	cp 144
+	jr c, .waitVBlank
+
+	xor a ; ld a, 0
+	ld [rLCDC], a
+	ret
+
+; Wait
+delay:
+	ld bc, 2000
+.loop
+	dec bc
+	ld a, b
+	or c
+	jr nz, .loop
+
+	ret
